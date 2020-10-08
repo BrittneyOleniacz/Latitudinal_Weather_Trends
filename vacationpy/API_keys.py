c9001e3142852c10907f7a739449c7e5f67985e8
Brittney Oleniacz <brittneyoleniacz@gmail.com>
2020-10-08 09:53:13 -0500

    ...

---
+++ vacationpy/API_keys.py
@@ -0,0 +1,4 @@
+# OpenWeatherMap API Key
+weather_api_key = "0b4e90fb6c07665ec8bc2b4ea8fb084e"
+# Google API Key
+g_key = "AIzaSyDGQaXouIcM2rhDr7RchORa-Wr5Hz3XHrk"
---
+++ weatherpy/API_keys.py
@@ -0,0 +1,4 @@
+# OpenWeatherMap API Key
+weather_api_key = "0b4e90fb6c07665ec8bc2b4ea8fb084e"
+# Google API Key
+g_key = "AIzaSyAi1wcLwLClQV7B5jCIkJ-LrcPIX3_y6CE"
